30cd8d8b1f897cfc1660d4b7bcc4c8f7099f1ca4
Gautham <gautham20111@gmail.com>
2018-03-07 05:03:21 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -17,13 +17,16 @@ setInterval(function(){
 		data[check()]=matrix;
 	}
 	console.log(Object.keys(data).length);}
-},3000)
+},1000)
 router.get('/', ensureAuthenticated, function(req, res){
 	res.json({length:Object.keys(data).length,matrix:matrix,flag:flag2});
 });
-router.get('/matrix', ensureAuthenticated, function(req, res){
+router.get('/data', ensureAuthenticated, function(req, res){
 	res.json(data);
 });
+router.get("/matrix",(req,res)=>{
+	res.json({matrix:data[req.query.d]})
+})
 
 function ensureAuthenticated(req, res, next){
 next();
@@ -49,14 +52,14 @@ function updatematrix() {
 }
 function check() {
 	var string=""
-	for (var k = 0; k < 15; k++) {
+	for (var k = 0; k < 16; k++) {
 		value=k&15;
 		sum=matrix[0][value&3]
 	for (var i = 0; i < matrix.length; i++) {
 		sum=sum&matrix[i][value&3];
 		value=parseInt(value/2)
 	}
-	string+=sum;
+	string+=sum==0?0:1;
 	}
 return string
 }
