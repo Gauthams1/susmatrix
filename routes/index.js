2f74201be6873273118fb63134c64a826aceef46
Gautham <gautham20111@gmail.com>
2018-03-07 13:55:21 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -5,35 +5,32 @@ var matrix=[
 	[0,0,0],
 	[0,0,0],
 	[0,0,0],
-	[1,1,1]
+	[0,0,0]
 ]
 flag2=false;
 data={}
 // Get Homepage
 setInterval(function(){
 	if(!flag2)
-{	for (var i = 0; i < 1000000; i++) {
-		updatematrix();
-		data[check()]=matrix;
+{	for (var i = 0; i < 100000; i++) {
+		data[check()]=JSON.stringify(matrix);
+			updatematrix();
 	}
-	console.log(Object.keys(data).length);}
-},3000)
-router.get('/', ensureAuthenticated, function(req, res){
+	}
+},300)
+router.get('/', function(req, res){
 	res.json({length:Object.keys(data).length,matrix:matrix,flag:flag2});
 });
-router.get('/test', ensureAuthenticated, function(req, res){
+router.get('/test', function(req, res){
 	res.render('index')
 });
-router.get('/data', ensureAuthenticated, function(req, res){
+router.get('/data', function(req, res){
 	res.json(data);
 });
 router.get("/matrix",(req,res)=>{
 	res.json({matrix:data[req.query.d]})
 })
 
-function ensureAuthenticated(req, res, next){
-next();
-}
 function updatematrix() {
 	var flag=false;
 	for (var i = 0; i < matrix.length; i++) {
@@ -61,7 +58,7 @@ function check() {
 	for (var i = 0; i < matrix[0].length; i++) {
 		sum=sum&matrix[value&3][i];
 		value=parseInt(value/2)
-	}
+		}
 	string+=sum==0?0:1;
 	}
 return string
