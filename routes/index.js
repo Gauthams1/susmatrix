26daae5f4a56ac1ce88f98b664715a3365187b98
Gautham <gautham20111@gmail.com>
2018-03-09 09:09:06 +0530

    yay

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/datas');
 var matrix=[
-	[1,1,1],[1,1,1],[1,1,1],[1,1,1]
+	[0,0],
+	[0,0],
+	[0,0],
+	[0,0],
 ]
 flag2=false;
 start = true;
-
+var data={}
+var dataset=matrix[0].length;
+dataset=Math.pow(2,dataset+1)-1;
 setInterval(function(){
 	if(!flag2)
-{	for (var i = 0; i < 100000; i++) {
-		data[check()]=JSON.stringify(matrix);
+{	for (var i = 0; i < 10000; i++) {
+		data[check(dataset)]=JSON.stringify(matrix);
 			updatematrix();
 	}
 	}
-},300)
+},3)
 router.get('/', function(req, res){
 	res.json({length:Object.keys(data).length,matrix:matrix,flag:flag2});
 
@@ -33,8 +39,10 @@ function updatematrix() {
 	for (var i = 0; i < matrix.length; i++) {
 		for (var j = 0; j < matrix[i].length; j++) {
 			matrix[i][j]+=1;
-			if(matrix[i][j]==8)
+			if(matrix[i][j]==2)
 			{matrix[i][j]=0}
+			else if((i+1==matrix.length)&&(j+1==matrix[0].length))
+			flag2=true
 			else {
 				flag=true
 				break;
@@ -42,21 +50,19 @@ function updatematrix() {
 		}
 		if(flag)
 		break;
-		if((i+1==matrix.length)&&(j==matrix[0].length))
-		flag2=true
-		}
+	}
 	return matrix
 }
-function check() {
+function check(number) {
 	var string=""
-	for (var k = 0; k < 16; k++) {
-		value=k&15;
+	for (var k = 0; k <= number; k++) {
+		value=k;
 		sum=matrix[value&3][0]
 	for (var i = 0; i < matrix[0].length; i++) {
-		sum=sum&matrix[value&3][i];
+		sum=sum*matrix[value&3][i];
 		value=value>>1
 		}
-	string+=sum&7==0?0:1;
+	string+=sum;
 	}
 return string
 }
